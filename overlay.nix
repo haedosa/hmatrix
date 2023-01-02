a68ff48a10613d2ffb699020fd29e47b504e9938
JJ Kim <jj@haedosa.xyz>
2023-01-02 22:32:09 +0900

    change haskellPackages.override to .extend

--- overlay.nix
+++ overlay.nix
@@ -1,17 +1,13 @@
 final: prev: with final; {
 
-  haskellPackages = prev.haskellPackages.override (old: {
-    overrides = lib.composeManyExtensions [
-                  (old.overrides or (_: _: {}))
-                  (haskell.lib.packageSourceOverrides {
-                    hmatrix = ./packages/base;
-                    hmatrix-glpk = ./packages/glpk;
-                    hmatrix-gsl = ./packages/gsl;
-                    hmatrix-sparse = ./packages/sparse;
-                    hmatrix-special = ./packages/special;
-                    hmatrix-tests = ./packages/tests;
-                  })
-                ];
-  });
+  haskellPackages = prev.haskellPackages.extend
+    (haskell.lib.packageSourceOverrides {
+      hmatrix = ./packages/base;
+      hmatrix-glpk = ./packages/glpk;
+      hmatrix-gsl = ./packages/gsl;
+      hmatrix-sparse = ./packages/sparse;
+      hmatrix-special = ./packages/special;
+      hmatrix-tests = ./packages/tests;
+    });
 
 }
