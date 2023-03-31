eb91b6cbd1641c7fcf2b1de7e1f8ec2c743e7f0a
JJ Kim <jj@haedosa.xyz>
2023-03-31 17:02:15 +0900

    rewrite haskell overrides to apply on all ghc's

--- overlay.nix
+++ overlay.nix
@@ -1,13 +1,32 @@
 final: prev: with final; {
 
-  haskellPackages = prev.haskellPackages.extend
-    (haskell.lib.packageSourceOverrides {
-      hmatrix = ./packages/base;
-      hmatrix-glpk = ./packages/glpk;
-      hmatrix-gsl = ./packages/gsl;
-      hmatrix-sparse = ./packages/sparse;
-      hmatrix-special = ./packages/special;
-      hmatrix-tests = ./packages/tests;
-    });
+
+  haskell = let
+    hlib = let oldLib = prev.haskell.lib; in oldLib // {
+      configLLVM = drv: oldLib.appendConfigureFlag drv "--ghc-options=-fllvm";
+      enableLLVM = drv: addLLVM (configLLVM drv);
+      addLLVM = drv: llvm: drv.overrideAttrs (old: {
+        buildInputs = old.buildInputs ++ [ llvm ];
+      });
+      enableProf = drv: oldLib.enableExecutableProfiling (oldLib.enableLibraryProfiling drv);
+      overridesOn = hlist: f: (hfinal: hprev: (__listToAttrs (map (name:
+        { inherit name; value = f hprev."${name}";} )
+        hlist)));
+    };
+    packageOverrides = lib.composeManyExtensions [
+      (prev.haskell.packageOverrides or (_: _: {}))
+      (hfinal: hprev: {
+        threadscope = hlib.doJailbreak (hlib.markUnbroken (hprev.threadscope));
+      })
+      (prev.haskell.lib.packageSourceOverrides {
+        hmatrix = ./packages/base;
+        hmatrix-glpk = ./packages/glpk;
+        hmatrix-gsl = ./packages/gsl;
+        hmatrix-sparse = ./packages/sparse;
+        hmatrix-special = ./packages/special;
+        hmatrix-tests = ./packages/tests;
+      })
+    ];
+  in prev.haskell // { lib = hlib; inherit packageOverrides; };
 
 }
